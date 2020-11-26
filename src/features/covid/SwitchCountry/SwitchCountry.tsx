5d7e136c8e35cb31d226b407dcaa80780c34a3c6
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-11-26 13:24:48 +0900

    create switch country component

---
+++ src/features/covid/SwitchCountry/SwitchCountry.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { makeStyles } from "@material-ui/core/styles"
+import { NativeSelect, FormControl } from "@material-ui/core"
+
+import { useDispatch } from "react-redux"
+import { fetchAsyncGetDaily } from "../covidSlice"
+
+
+const useStyles = makeStyles((theme) => ({
+    fromControl: {
+        marginBottom: theme.spacing(3),
+        minWidth: 320,
+    }
+}))
+
+
+
+const SwitchCountry: React.FC = () => {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+
+    const countries = [
+        "japan",
+        "china",
+        "us",
+        "france",
+        "italy",
+        "spain",
+        "united kingdom",
+        "germany",
+        "russia",
+        "brazil",
+        "taiwan",
+        "thailand",
+        "new zealand",
+        "sweden",
+        "india",
+    ]
+
+
+    return (
+        <FormControl className={classes.fromControl}>
+            <NativeSelect
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                    dispatch(fetchAsyncGetDaily(e.target.value))
+                } 
+            >
+                {countries.map((country, i) => (
+                    <option key={i} value={country}>
+                        {country}
+                    </option>
+                ))}
+            </NativeSelect>
+        </FormControl>
+    )
+}
+
+export default SwitchCountry
